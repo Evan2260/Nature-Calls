f5d483a0a1461ab5ca1911aed62cf5ef4fe6ccb4
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-21 16:15:33 -0500

    Created Map Container component

---
+++ app/javascript/react/components/MapContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { GoogleMap, Marker, withScriptjs, withGoogleMap } from 'react-google-maps'
+
+  const MapContainer = withScriptjs(withGoogleMap((props) => {
+
+      return(
+      <>
+        <GoogleMap
+          defaultZoom={10}
+          defaultCenter={{ lat:42.3601, lng:-71.0589 }}
+        >
+        {props.isMarkerShown && <Marker position={{ lat:42.3601, lng:-71.0589 }} />}
+        </GoogleMap>
+      </>
+    )
+  }));
+
+export default MapContainer
