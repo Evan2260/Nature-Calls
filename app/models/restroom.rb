fae996bb4f768b925299a3d5ab7e6236f95a6a26
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-03-07 19:32:53 -0500

    Added to validationsin restroom model

--- app/models/restroom.rb
+++ app/models/restroom.rb
@@ -1,12 +1,12 @@
 class Restroom < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 
   validates :business_name, presence: true
   validates :address, presence: true, uniqueness: true
   validates :sanitation, presence: true
   validates :restroom_image, presence: true
-  validates :payment?
-  validates :paying_customer?
-  validates :employee_only?
-  validates :handicap_stall?
+  validates :payment?, allow_blank: true
+  validates :paying_customer?, allow_blank: true
+  validates :employee_only?, allow_blank: true
+  validates :handicap_stall?, allow_blank: true
 end
